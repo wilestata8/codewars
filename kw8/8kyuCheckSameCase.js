0f8ca42a6936992ed09f1ab49f3a01da9df44b7a
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-05-26 17:33:36 -0400

    Add files via upload

---
+++ kw8/8kyuCheckSameCase.js
@@ -0,0 +1,9 @@
+//Instructions
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+
+//Answer
+sameCase = (a,b) => /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)) : -1
