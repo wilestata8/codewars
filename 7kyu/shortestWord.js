b19254b48ac87a40c189bcfb215cc617a129f1e6
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-07-12 19:51:55 -0400

    Add files via upload

--- 7kyu/shortestWord.js
+++ 7kyu/shortestWord.js
@@ -1,10 +1,22 @@
 //Directions
-/*Simple, given a string of words, return the length of the shortest word(s).
+/*Given an array of ones and zeroes, convert the equivalent binary value to an integer.
 
-String will never be empty and you do not need to account for different data types.*/
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.*/
+
+//Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
 
 
 
 //Answer
-let arr = s.split(" ").sort((a, b) => a.length - b.length);
-return arr[0].length
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2);
+};
