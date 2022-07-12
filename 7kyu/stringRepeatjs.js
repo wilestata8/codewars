7632ec1298ad73ffce05c55fb0d248131a2613fd
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-07-12 19:56:52 -0400

    Add files via upload

---
+++ 7kyu/stringRepeatjs.js
@@ -0,0 +1,12 @@
+//Directions
+/*Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.*/
+
+
+// Examples
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+//Answer
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
