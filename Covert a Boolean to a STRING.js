0c0d27f1f4ef25415ca38cb37a5434c4f83e28eb
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-07-11 22:38:28 -0400

    Add files via upload

---
+++ Covert a Boolean to a STRING.js
@@ -0,0 +1,9 @@
+//Directions
+/*Implement a function which convert the given boolean value into its string representation.*/
+
+
+//Answer
+function booleanToString(b){
+  const string = b.toString();
+  return string 
+}
