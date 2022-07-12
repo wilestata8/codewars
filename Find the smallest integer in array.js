36dad9bf686fb017e0984dfccdd757c03997033b
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-07-11 22:57:16 -0400

    Update Find the smallest integer in array.js

--- Find the smallest integer in array.js
+++ Find the smallest integer in array.js
@@ -11,13 +11,8 @@ Given [34, -345, -1, 100] your solution will return -345
 
 //*********Answer*************
 class SmallestIntegerFinder {
-  findSmallestInt(arr) {
-    let smallest = arr[0]
-  
-      for(let i = 0; i < arr.length; i++){
-          if(arr[i] < smallest) smallest = arr[i]
-      }
-  
-      return smallest
+ findSmallestInt(args) {
+    return Math.min(...args)
   }
+}
   }
