500f6ec98783b9544380326f8af6bda42d6fde88
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-07-13 19:17:36 -0400

    Add files via upload

---
+++ 7kyu/template strings.js
@@ -0,0 +1,12 @@
+//directions
+/*Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.*/
+
+
+// answers
+let TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`;
+}
