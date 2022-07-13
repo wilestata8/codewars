703c78ae8df620591ca191f5762e94922294c0d0
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-07-12 23:33:02 -0400

    Update calculateAverage.js

--- 7kyu/calculateAverage.js
+++ 7kyu/calculateAverage.js
@@ -3,5 +3,13 @@
 
 
 //Answer
-const find_average = (arr) => !arr || arr.length === 0 ? 0
-:arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
+function find_average(array) {
+  if (array.length === 0) {
+  return 0;
+  }
+  var result = 0;
+  for (i=0; i<array.length; i++) {
+    result +=array[i];
+  }
+  return result/array.length;
+}
