a64e025b3fa8972db636a463a84ed0f890de23ba
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-08-02 13:31:36 -0400

    Add files via upload

---
+++ 7kyu/mumbling.js
@@ -0,0 +1,24 @@
+//Directions
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+
+//Answers
+function accum(s) {
+  const result = []
+    const lowerStr = s.toLowerCase()
+  
+    for(let i = 0; i < lowerStr.length; i++) {
+      let str = lowerStr[i].toUpperCase()
+      for(let j = 0; j < i; j++) {
+        str += lowerStr[i]
+      }
+      result.push(str)
+    }
+  
+    return result.join("-")
+  }
