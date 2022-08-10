f9fe8a90c9de73a345e8baf860caf733f291232b
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-08-09 20:04:52 -0400

    Add files via upload

---
+++ kw8/convertBooleanValuesToString.js
@@ -0,0 +1,9 @@
+// directions
+/* Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. */
+
+
+//Answer
+function boolToWord( bool ){
+  const str = bool === true ? "Yes" : "No"
+  return str
+    }
