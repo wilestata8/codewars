ef933e368c36f40f5a4ce6f72b9815c7f0dfb9bd
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-08-11 11:21:57 -0400

    Add files via upload

---
+++ kw8/oppositeNumber.js
@@ -0,0 +1,13 @@
+//direction
+/* 
+Very simple, given an integer or a floating-point number, find its opposite. */
+
+//Example
+1: -1
+14: -14
+-34: 34
+
+// Answer
+function opposite(number) {
+  return(-number);
+}
