e880fbd3f0f970dd3aa098450c8d1c25dfa16a83
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-08-22 16:16:34 -0400

    Add files via upload

---
+++ 5KYU/extractTheDomainName.js
@@ -0,0 +1,18 @@
+//directions
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+//Answers
+function domainName(url){
+  let sourceString = url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
+   let domain = sourceString.split(".")[0];
+   return domain;
+}
+
+answer 2
+function domainName(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
