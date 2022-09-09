3df1c5a64f434f2e998c7034f59b1a04ccdb46eb
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-09-08 20:54:11 -0500

    Add files via upload

---
+++ 7kyu/regexValidatePinCode.js
@@ -0,0 +1,9 @@
+// Directions
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+//Answers
+function validatePIN (pin) {
+  return /^(\d{4}|\d{6})$/.test(pin);
+}
